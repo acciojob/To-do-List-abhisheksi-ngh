f717ec819210d0dd6f6458afa0dfb90eea7fc325
A <abhishekkumarsingh9585@gmail.com>
2025-05-10 00:20:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
+document.addEventListener("DOMContentLoaded", ()=>{
+	const newTodoInput=document.getElementById("newTodoInput");
+	const btn = document.getElementById("addTodoBtn");
+	const list = document.getElementById("todoList");
+
+	btn.addEventListener('click', ()=>{
+		const input = newTodoInput.value.trim();
+		if(input!==""){
+			const li= document.createElement("li");
+			li.textContent= input;
+			list.appendChild(li);
+			newTodoInput.value="";
+		}
+	})
+})
 
